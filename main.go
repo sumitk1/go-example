c02b2bb6d10ae2c2b3935303bb18a8325b29a734
Sumit Kumar <sumitk.85@gmail.com>
2017-07-02 11:04:30 -0700

    fixing the Fprint

--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
+
 import (
-     "os"
-     "log"
-     "fmt"
-     "net/http"
+	"fmt"
+	"net/http"
 )
-func handler (w http.ResponseWriter, r *http.Request) {
-    log.Println("Received Request: ", r.URL.Path)
-    fmt.Sprintf(w, "Hello World!")
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "yoo people wassup!")
 }
-func main () {
-    http.HandleFunc("/", handler)
-   
-    port := os.Getenv("PORT")
-    if port == "" {
-        log.Fatal("PORT environment variable was not set")
-    }
-    err := http.ListenAndServe(":"+port, nil)
-    if err != nil {
-        log.Fatal("Could not listen: ", err)
-    }
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	http.ListenAndServe(":8080", nil)
 }
