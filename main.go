836eebc9d3cc396eb34832ab24e13eac84baf4ec
Sumit Kumar <sumitk.85@gmail.com>
2017-07-11 23:46:50 -0700

    OOP through struct

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 	"./handlers"
 )
 
+// OOP implementation
+type person struct {
+	fname string
+	lname string
+}
+
+func (p *person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("helloooo oop func! fname = " + p.fname + " -- lname = " + p.lname))
+}
+
 func echo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "yoo people wassup!")
 }
@@ -15,5 +25,8 @@ func main() {
 	http.HandleFunc("/", echo)
 	http.HandleFunc("/user/", handlers.UrlParams)
 
-	http.ListenAndServe(":4080", nil)
+	person_handler := &person{fname: "my_first_name", lname: "my_last_name"}
+	//http.HandleFunc("/oop/", person_func)
+
+	http.ListenAndServe(":4080", person_handler)
 }
