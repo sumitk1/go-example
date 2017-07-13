a5843a45068d865e56c186ff10fcfe6caccd2e2c
Sumit Kumar <sumitk.85@gmail.com>
2017-07-12 22:29:39 -0700

    Working template example

--- handlers/funcs.go
+++ handlers/funcs.go
@@ -2,10 +2,24 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func UrlParams(w http.ResponseWriter, r *http.Request) {
 	// spit out the path
 	fmt.Fprintln(w, r.URL.Path[:])
+}
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p := &Page{Title: title}
+	p.Body = []byte("some good body!!")
+	t, _ := template.ParseFiles("../view/template.html")
+	t.Execute(w, p)
 }
--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
+
+	"log"
 	"net/http"
 
 	"./handlers"
 )
 
 // OOP implementation
+// only accessible inside this class - to make it global use Person
 type person struct {
 	fname string
 	lname string
 }
 
+// Person is receiving this ServeHTTP() method
 func (p *person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("helloooo oop func! fname = " + p.fname + " -- lname = " + p.lname))
 }
@@ -23,10 +27,14 @@ func echo(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", echo)
+	log.Println("some log1 ... ")
 	http.HandleFunc("/user/", handlers.UrlParams)
+	log.Println("some log2 ... ")
+	http.HandleFunc("/edit/", handlers.editHandler)
 
-	person_handler := &person{fname: "my_first_name", lname: "my_last_name"}
-	//http.HandleFunc("/oop/", person_func)
+	//person_handler := &person{fname: "my_first_name", lname: "my_last_name"}
+	//http.HandleFunc("/oop/", &person{fname: "my_first_name", lname: "my_last_name"})
 
-	http.ListenAndServe(":4080", person_handler)
+	log.Println("some log ... ")
+	http.ListenAndServe(":4080", nil)
 }
